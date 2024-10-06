e41cdb766fcf518c239acb99d8611b36d87100cf
tencho <ytk025@gmail.com>
2024-10-06 18:39:20 +0900

    UIアイコンの不要なリサイズを削除

--- Assets/Scripts/Game/Hud/HudItemCountView.cs
+++ Assets/Scripts/Game/Hud/HudItemCountView.cs
@@ -10,15 +10,6 @@ namespace AntColony.Game.Hud
         [SerializeField] private Image icon;
         [SerializeField] private TextMeshProUGUI count;
 
-        private void Start()
-        {
-            // アイコンのリサイズ
-            Vector2 frameSize = icon.rectTransform.sizeDelta;
-            Vector2 imageSize = icon.sprite.textureRect.size;
-            Vector2 resized = RectTools.ResizeVector2(imageSize, frameSize, ResizeMode.Contain);
-            icon.rectTransform.sizeDelta = resized;
-        }
-
         public void SetCount(int number)
         {
             count.text = number.ToString();
